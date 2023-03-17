3486cab1da98fa7c387a682dbdc760d1842b3fe3
Devan <devan.balaguer@gmail.com>
2023-03-17 14:09:12 -0400

    Fixed future curtain, accent color, 3rd image

--- html/script.js
+++ html/script.js
@@ -1,3 +1,10 @@
+var curtain1 = document.getElementById("curtain1");
+var curtain2 = document.getElementById("curtain2");
+
+function changeClass(){
+  curtain1.className += " move-right";
+  curtain2.className += " move-left";
+}
 
 function reveal() {
     var reveals = document.querySelectorAll(".reveal");
